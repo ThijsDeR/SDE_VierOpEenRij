1755ebbb4c798c8bfe6a3cc70db08469c55509af
Ivy Dekker <ivy_dk@hotmail.com>
2023-01-19 15:05:29 +0100

    methodHandler

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.lang.reflect.Method;
+import java.sql.Struct;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void start() {
         System.out.println("Hello world!");
     }
 }
